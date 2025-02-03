201d279875dbe4c3ee9d971265f1eaa92fb5b68f
Simon <szhang0709@gmail.com>
2025-02-03 15:29:42 +0100

    fix: changed first valid date

--- src/domain/quizz/isCardDateValid.test.ts
+++ src/domain/quizz/isCardDateValid.test.ts
@@ -1,19 +1,21 @@
-import { expect, test } from "bun:test";
+import { describe, expect, test } from "bun:test";
 import { Card } from "@/domain/cards/Card";
 import { CardCategory } from "@/domain/cards/CardCategory";
 import { CardUserData } from "@/domain/cards/CardUserData";
 import { isCardDateValid } from "@/domain/quizz/isCardDateValid";
 
-test("card should be valid when card is category 1 and date is today", () => {
-  const cardDomainObject = new Card(
-    "6c10ad48-2bb8-4e2e-900a-21d62c00c07b",
-    new CardUserData(
-      "What is hexagonal architecture?",
-      "It's a design pattern for organizing code.",
-      "architecture",
-    ),
-    CardCategory.First,
-  );
+describe("isCardDateValid", () => {
+  test("card should be valid when card was created yesterday and date is today", () => {
+    const cardDomainObject = new Card(
+      "6c10ad48-2bb8-4e2e-900a-21d62c00c07b",
+      new CardUserData(
+        "What is hexagonal architecture?",
+        "It's a design pattern for organizing code.",
+        "architecture",
+      ),
+      CardCategory.First,
+    );
 
-  expect(isCardDateValid(cardDomainObject));
+    expect(isCardDateValid(cardDomainObject));
+  });
 });
--- src/domain/quizz/isCardDateValid.ts
+++ src/domain/quizz/isCardDateValid.ts
@@ -1,5 +1,21 @@
 import type { Card } from "../cards/Card";
 
-export function isCardDateValid(card: Card) {
-  return card.lastUpdateDate <= new Date();
+export function isCardDateValid(card: Card): boolean {
+  const today = new Date();
+  const cardDate = getCardNextQuizzDate(card);
+
+  return (
+    today.getDate() === cardDate.getDate() &&
+    today.getMonth() === cardDate.getMonth() &&
+    today.getFullYear() === cardDate.getFullYear()
+  );
+}
+
+function getCardNextQuizzDate(card: Card): Date {
+  const CATEGORY_WAITING_TIME = 1;
+
+  const cardDate = new Date(card.lastUpdateDate);
+  cardDate.setDate(cardDate.getDate() + CATEGORY_WAITING_TIME);
+
+  return cardDate;
 }
